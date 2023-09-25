445d81807c671a2915c9b13d0cb4fbd8bb41649f
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2023-09-25 10:09:46 -0700

    update lessons

---
+++ Javascript/starting.js
@@ -0,0 +1,27 @@
+//js running top to buttom
+
+console.log(1);
+console.log(2);
+
+// variable
+// let allow us to override variable
+
+let age = 25;
+let year = 2019
+console.log(age,year)
+
+// const
+// const cannot be override. And it will cause error if we try to override them
+const string= "cannot override";
+console.log(string)
+
+// var
+// var can be override, similar to let, older way to make variable
+var score = 75;
+console.log(score)
+
+
+
+
+
+
