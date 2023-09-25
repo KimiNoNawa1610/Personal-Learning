622e9d9784248728ed3ad5b37cd9e24c029ab0cf
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2023-09-25 10:34:37 -0700

    update js lessons

--- Javascript/starting.js
+++ Javascript/starting.js
@@ -20,8 +20,30 @@ console.log(string)
 var score = 75;
 console.log(score)
 
+// there are reserve keywords in js. Following the link to find more detail: https://www.w3schools.com/js/js_reserved.asp
 
+/*----------------------------------------------------------------------------------------------------------------------- */
 
+//Data Types: https://www.programiz.com/javascript/data-types
 
+// STRING
 
+console.log("Hello World")
+let email = "email@email.com"
+console.log(email)
 
+// string concatination
+let first_name = "Brandon"
+let last_name = "Anderson"
+let full_name = first_name+" "+last_name
+console.log(full_name)
+
+// extract character from a string
+console.log(full_name[1])
+
+// String methods/ properties
+console.log(full_name.length) // find the length of the string
+console.log(full_name.toLocaleUpperCase()) // make the string to upper case
+console.log(full_name.toLocaleLowerCase()) // make the string to lower case
+console.log(typeof(full_name)) // get what type of the full_name variable
+console.log(email.indexOf("@")) // get index of a character from a string
