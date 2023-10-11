c104e0ce97fc6fb57ee1a7b87da2fbece424f42a
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2023-10-11 14:24:29 -0700

    update dom

--- Javascript/DocumentObjectModel.js
+++ Javascript/DocumentObjectModel.js
@@ -1,5 +1,15 @@
 // Document Object Model (DOM)
 
 const para = document.querySelector("p"); // goes into the document, from top to bottom and grab the first p tag
-
 console.log(para)
+
+const err = document.querySelector("div.error") // only select the first div with the class error
+console.log(err)
+
+const multi_para = document.querySelectorAll("p") // this will select all p tag and stored as a collection
+console.log(multi_para)
+
+multi_para.forEach((para,index)=>{// using multi_para with forEach method
+    console.log(para,index)
+})
+
